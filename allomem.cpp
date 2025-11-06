359bf5d2707695c7efc546afe79c892da4d21b85
11249a095-prog <11249a095@kanchiuniv.ac.in>
2025-11-05 23:51:47 -0800

    Add files via upload

---
+++ allomem.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter number of elememts:";
+    cin>>n;
+    int *arr= new int[n];
+    cout<<"Enter "<<n<<" elements:\n";
+    for(int i=0;i<n;i++)
+    cin>>arr[i];
+    cout<<"\nElements are:";
+    for(int i=0;i<n;i++)
+    cout<<arr[i]<<" ";
+    delete[] arr;
+    return 0;
+}
